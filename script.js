a93db09e0ed12bdeb2ea26062196cf2ac6ee9468
Esad Abdullah Kösedağ <81075846+kaesit@users.noreply.github.com>
2023-04-16 21:37:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.getElementsByTagName("li")[0].setAttribute("unselectable", "on");
